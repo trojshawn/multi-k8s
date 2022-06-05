b5ade165221635b5544e93ecb20903edea18092d
trojshawn <shawn.mccaughan@troj.ai>
2022-06-05 15:45:19 -0300

    edited page header

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Fib Calc</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
